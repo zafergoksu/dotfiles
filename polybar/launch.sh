68ba54c10f2b75ed15f925befa24b361230f7ffc
zgoksu <zafergoksu00@gmail.com>
2020-03-17 18:05:31 +0000

    Reverted polybar

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,6 +1,4 @@
-#!/usr/bin/env sh
-
-## Add this to your wm startup file.
+#!/bin/bash
 
 # Terminate already running bar instances
 killall -q polybar
@@ -8,5 +6,8 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-polybar -c ~/.config/polybar/config.ini main &
+# Launch Polybar, using default config location ~/.config/polybar/config
+polybar top &
+
+echo "Polybar launched..."
+
