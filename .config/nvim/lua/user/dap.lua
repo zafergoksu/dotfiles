f6931f2e918c39da99298afb5510817e7876ea78
zgoksu <zafergoksu00@gmail.com>
2022-09-06 21:46:03 +0100

    Update DAP config to include csharp/.NET

--- .config/nvim/lua/user/dap.lua
+++ .config/nvim/lua/user/dap.lua
@@ -83,3 +83,20 @@ dap.configurations.cpp = {
 }
 
 dap.configurations.c = dap.configurations.cpp
+
+dap.adapters.coreclr = {
+    type = "executable",
+    command = "/usr/bin/netcoredbg",
+    args = { "--interpreter=vscode" }
+}
+
+dap.configurations.cs = {
+    {
+        type = "coreclr",
+        name = "launch - netcoredbg",
+        request = "launch",
+        program = function()
+            return vim.fn.input("Path to dll", vim.fn.getcwd() .. "/bin/Debug/", "file")
+        end,
+    }
+}
