69fb8a9725fd8d2c3f061e1ace75eb01db63689d
zgoksu <zafergoksu00@gmail.com>
2022-10-02 01:34:38 +0100

    Update nvim config

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -50,7 +50,7 @@ return packer.startup(function(use)
     use "JoosepAlviste/nvim-ts-context-commentstring"
     use "kyazdani42/nvim-web-devicons"
     use "kyazdani42/nvim-tree.lua"
-    use { "akinsho/bufferline.nvim", tag = "v1.*" }
+    use "akinsho/bufferline.nvim"
     use "moll/vim-bbye"
     use "nvim-lualine/lualine.nvim"
     use "arkav/lualine-lsp-progress"
