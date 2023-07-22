9338ef58968dd99e7b50c4917e68bf8dae5cf707
zgoksu <zafergoksu00@gmail.com>
2023-07-22 04:10:51 +0100

    update nvim configs

--- .config/nvim/lua/user/cmp.lua
+++ .config/nvim/lua/user/cmp.lua
@@ -70,8 +70,8 @@ cmp.setup {
                 cmp.select_next_item()
             elseif luasnip.expandable() then
                 luasnip.expand()
-            elseif luasnip.expand_or_jumpable() then
-                luasnip.expand_or_jump()
+                --[[ elseif luasnip.expand_or_jumpable() then ]]
+                --[[     luasnip.expand_or_jump() ]]
             elseif check_backspace() then
                 fallback()
             else
